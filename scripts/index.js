c190b7c21eb714db9fdffaddb5e847647d938f5f
Adrian Mayfield <amay0808@protonmail.com>
2023-03-05 05:54:56 -0800

    new querySelectors

--- scripts/index.js
+++ scripts/index.js
@@ -27,10 +27,17 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
   profileEditModal.classList.add("modal_opened");
 });
+
 const modalCloseButton = document.querySelector("#modal-close-button");
 modalCloseButton.addEventListener("click", () => {
   profileEditModal.classList.remove("modal_opened");
