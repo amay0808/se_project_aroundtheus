fa519f4f535b836cbd06b0893735c50031d3ac3f
Adrian Mayfield <amay0808@protonmail.com>
2023-03-06 00:46:49 -0800

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -24,9 +24,10 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
-
+//elements
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
@@ -34,22 +35,27 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+// Functions
 function closepopup() {
   profileEditModal.classList.remove("modal_opened");
 }
-profileEditButton.addEventListener("click", () => {
-  profileTitleInput.value = profileTitle.textContent;
-  profileEditModal.classList.add("modal_opened");
-});
+//Event Handlers
 
-const modalCloseButton = document.querySelector("#modal-close-button");
-modalCloseButton.addEventListener("click", () => {
-  closepopup();
-});
-profileEditForm.addEventListener("submit", (e) => {
+function handleProfileEditSubmit(e) {
   e.preventDefault();
   console.log("form submitted");
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   closepopup();
+}
+
+// Event Listeners
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileEditModal.classList.add("modal_opened");
 });
+
+modalCloseButton.addEventListener("click", closepopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
