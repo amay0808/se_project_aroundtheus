c5c20f9b1574b6a97362c069e9c24dc7dd664e96
Adrian Mayfield <amay0808@protonmail.com>
2023-03-24 02:27:54 -0700

    like button

--- scripts/index.js
+++ scripts/index.js
@@ -61,6 +61,19 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  //find delte button
+
+  //add event listner to the delete button
+  // cardElement.remove();
+
+  //add click listner to the cardImage element
+
+  //openModal withpreviewImagModal
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
   cardTitleEl.textContent = cardData.name;
@@ -115,8 +128,3 @@ addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
 
 initialCards.forEach((cardData) => renderCard(cardData, cardListEl));
 const likeButtons = document.querySelectorAll(".card__like-button");
-likeButtons.forEach((likeButton) => {
-  likeButton.addEventListener("click", () => {
-    likeButton.classList.toggle("card__like-button_active");
-  });
-});
